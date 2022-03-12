eef555a590046926532056e64b8e8252898816d2
prakash chandra muduli <prakashchandramuduli007@gmail.com>
2022-03-13 00:08:23 +0530

    api add test mode

--- script/script.js
+++ script/script.js
@@ -1,14 +1,43 @@
 window.onload =init;
 function init(){
-    searchbtn = document.getElementById('search-btn');
-    searchbtn.addEventListener('click',()=>{
-        getapi();
+    const searchData = document.getElementById("search-input");
+    const searchBtn = document.getElementById('search-btn');
+   
+    loading_item = document.getElementById('loadtime');
+    searchBtn.addEventListener('click',()=>{
+        if(searchData.value==''){
+            alert('Please type in a search term');
+        }
+        else{
+            getapi(searchData.value)
+            
+            
+        }
+        
     })
 }
 
-function getapi(){
-    fetch('http://example.com/movies.json')
+function getapi(search_term){
+    fetch(`https://api.lyrics.ovh/suggest/${search_term}`)
+    
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data =>{populate(data)
+     console.log(data.data)} )
+    
+    .catch(console.log('error'));
   
 }
+function populate(data) {
+    console.log('populating data');
+    let arr_len =data.data.length;
+    const item_div = document.getElementById('items-containers');
+    console.log('artist name: ' + data.data[0].artist.name);
+    for(let i = 0; i < arr_len; i++){
+        item_div.innerHTML+=`<div class="items">
+        <h2> ${data.data[i].artist.name} </h2>
+        <h3>- ${data.data[i].album.title}</h3>
+        <button class="getLink">Get Lyrics</button>
+    </div>`;
+    }
+}
+
