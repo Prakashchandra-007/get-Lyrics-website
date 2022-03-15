e4075f1698b342a7faf331a7afa038c1492f3383
prakash chandra muduli <prakashchandramuduli007@gmail.com>
2022-03-15 18:31:18 +0530

    get lyric api done

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ window.onload =init;
 function init(){
     const searchData = document.getElementById("search-input");
     const searchBtn = document.getElementById('search-btn');
-   
+    
     loading_item = document.getElementById('loadtime');
     searchBtn.addEventListener('click',()=>{
         if(searchData.value==''){
@@ -16,28 +16,68 @@ function init(){
         
     })
 }
-
+//function get list of search items
+const url='https://api.lyrics.ovh';  // api endpoint
+const item_div = document.getElementById('items-containers');
+let song_list ;
 function getapi(search_term){
-    fetch(`https://api.lyrics.ovh/suggest/${search_term}`)
+    
+    fetch(`${url}/suggest/${search_term}`)
     
     .then(response => response.json())
     .then(data =>{populate(data)
-     console.log(data.data)} )
+     console.log(data.data)
+        song_list = data.data}
+     )
+
     
     .catch(console.log('error'));
   
 }
+
+//function for papulate list of items by song.
 function populate(data) {
+    
     console.log('populating data');
     let arr_len =data.data.length;
-    const item_div = document.getElementById('items-containers');
+    
     console.log('artist name: ' + data.data[0].artist.name);
     for(let i = 0; i < arr_len; i++){
-        item_div.innerHTML+=`<div class="items">
-        <h2> ${data.data[i].artist.name} </h2>
-        <h3>- ${data.data[i].album.title}</h3>
-        <button class="getLink">Get Lyrics</button>
-    </div>`;
+        item_div.innerHTML+=(`<div class="items">
+        <p><strong> ${data.data[i].artist.name}</strong>
+        - ${data.data[i].title} </p>
+        <button data-songId="${data.data[i].id}" onClick='getLyrics(this)' class="getLink">Get Lyrics</button>
+    </div>`);
     }
 }
 
+//function for get lyric of perticular song.
+function getLyrics(elem){
+    console.log(elem.getAttribute('data-songId'));
+    let id_song = elem.getAttribute('data-songId')
+    
+    
+    let i=0;
+    //loop for find peticular song object by id
+    for(; i<15 ; i++){
+        if(song_list[i].id==id_song){
+            break;
+        }
+    }
+    let title = song_list[i].title; //store song titile
+    let artist_name =song_list[i].artist.name; //store artist name
+    console.log(title+">>>" +artist_name);
+    lyricApi(artist_name,title)
+   
+}
+
+function lyricApi(artist_name,title){
+    let result;
+    fetch(`https://api.lyrics.ovh/v1/${artist_name}/${title}`)
+    .then(resp => resp.json())
+    .then(data=>{console.log(data.lyrics)
+        item_div.innerHTML =`<h1>${data.lyrics}</h1>`
+        //  console.log(data.lyrics)
+        });
+    
+}
